04229363ff45366596799f754394d3fc19d54784
Linda Blomkvist <3tomu24.blomli@folkuniversitetet.nu>
2024-11-07 10:38:27 +0100

    Lagt till Product

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import javax.xml.catalog.Catalog;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Catalog catalog = new Catalog();
     }
 }
